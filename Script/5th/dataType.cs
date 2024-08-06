fa5a8e8e16da9b8252acfd8836e2da58941ab86b
Hiroto-Aizawa <r_20love@icloud.com>
2024-08-06 21:04:15 +0900

    5th追加

---
+++ Script/5th/dataType.cs
@@ -0,0 +1,14 @@
+namespace DataType1
+{
+    using System;
+    public class Class1
+    {
+        public static int Main(string[] args)
+        {
+            int i=123;
+            string s = i.ToString();
+            Console.WriteLine( "i={0}, s={1}", i, s );
+            return 0;
+        }
+    }
+}
