0727dafb45f0b80a1bf2f94d358b1d18227ed380
Hiroto-Aizawa <r_20love@icloud.com>
2024-08-10 23:20:31 +0900

    7th 更新

--- Script/7th/CastAndDataConversion3.cs
+++ Script/7th/CastAndDataConversion3.cs
@@ -1,5 +1,7 @@
 namespace CastAndDataConversion1
 {
+    // https://atmarkit.itmedia.co.jp/fdotnet/csharp_abc/csharp_abc_007/csharp_abc03.html
+    
     using System;
     public class Class1
     {
@@ -22,6 +24,42 @@ namespace CastAndDataConversion1
             Console.WriteLine("{0}\n{1}", c.hello, c2.hello);
             // 出力結果
             // コンパイラ内部で数値がdouble型だと認識され、暗黙的に変化できないとエラーになる
+            return 0;
+        }
+    }
+}
+
+
+namespace CastAndDataConversion2
+{
+    using System;
+    public class Class2
+    {
+        public String hello;
+    }
+    public class Class1
+    {
+        public String hello;
+        public static int Main(string[] args)
+        {
+            // 【無関係の参照型へのキャスト】
+
+            
+            Class1 c = new Class1();
+            c.hello = "Hello!";
+            object o = (object)c;
+            // Class1とは無関係なクラスにキャストしている
+            Class2 c2 = (Class2)o;
+            // コンパイルではエラーは起こらないが、
+            // 実行すると例外が発生したというエラーメッセージが表示され強制終了する
+            Console.WriteLine("{0},{1}", c.hello, c2.hello);
+            // 出力結果
+            Unhandled Exception:
+            // System.InvalidCastException: Specified cast is not valid.
+            //   at ConsoleApplication11.Class1.Main (System.String[] args) [0x00013] in <fdc616ebbbca43c08f364d635b2f19ff>:0 
+            // [ERROR] FATAL UNHANDLED EXCEPTION: System.InvalidCastException: Specified cast is not valid.
+            //   at ConsoleApplication11.Class1.Main (System.String[] args) [0x00013] in <fdc616ebbbca43c08f364d635b2f19ff>:0 
+
             return 0;
         }
     }
