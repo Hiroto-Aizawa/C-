471439614bfbdafcd41010807080875cb45aba32
Hiroto-Aizawa <r_20love@icloud.com>
2024-08-21 23:21:21 +0900

    12th 更新

--- Script/12th/IndexerAndProperty1.cs
+++ Script/12th/IndexerAndProperty1.cs
@@ -8,10 +8,30 @@ namespace IndexerAndProperty1
     static void Main(string[] args)
     {
       string s = "Hello!";
-      for( int i=0; i<s.Length; i++ )
+      for (int i = 0; i < s.Length; i++)
       {
-        Console.WriteLine( s[i] );
+        Console.WriteLine(s[i]);
       }
+    }
+  }
+}
+
+namespace IndexerAndProperty2
+{
+  using System;
+
+  class Class1
+  {
+    static void Main(string[] args)
+    {
+      string s = "Hello!";
+      char[] a = { 'H', 'e', 'l', 'l', 'o', '!' };
+      Console.WriteLine(a.GetUpperBound(0));
+      //Console.WriteLine( s.GetUpperBound(0) );  // 'string' に 'GetUpperBound' の定義がありません。
+      Console.WriteLine(s.ToUpper());
+      //Console.WriteLine( a.ToUpper() ); // 'System.Array' に 'ToUpper' の定義がありません。
+      Console.WriteLine(s.GetType().FullName);
+      Console.WriteLine(a.GetType().FullName);
     }
   }
 }
