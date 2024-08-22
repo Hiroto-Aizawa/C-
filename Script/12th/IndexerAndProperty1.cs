477a776e8bd854bdbf0e11bd3916b8b3f3022e98
Hiroto-Aizawa <r_20love@icloud.com>
2024-08-22 21:31:20 +0900

    12th 追加

--- Script/12th/IndexerAndProperty1.cs
+++ Script/12th/IndexerAndProperty1.cs
@@ -1,17 +1,29 @@
-
-using System;
+// https://atmarkit.itmedia.co.jp/fdotnet/csharp_abc/csharp_abc_012/csharp_abc01.html
 
 namespace IndexerAndProperty1
 {
+  using System;
+
   class Class1
   {
     static void Main(string[] args)
     {
+      // 【インデクサとは何か】
+
       string s = "Hello!";
+
       for (int i = 0; i < s.Length; i++)
       {
+        // 文字列にアクセスしているように見えるが、実はインデクサにアクセスしている
         Console.WriteLine(s[i]);
       }
+      // 出力結果
+      // H
+      // e
+      // l
+      // l
+      // o
+      // !
     }
   }
 }
@@ -24,14 +36,27 @@ namespace IndexerAndProperty2
   {
     static void Main(string[] args)
     {
-      string s = "Hello!";
+      // 【インデクサと配列の違い】
+
+      // オブジェクトを配列のように扱いアクセス可能にしている
       char[] a = { 'H', 'e', 'l', 'l', 'o', '!' };
+      string s = "Hello!";
+
+      // 配列の最大インデックスを取得する
       Console.WriteLine(a.GetUpperBound(0));
       //Console.WriteLine( s.GetUpperBound(0) );  // 'string' に 'GetUpperBound' の定義がありません。
+
+      // 現在の文字列を大文字にしたもの
       Console.WriteLine(s.ToUpper());
       //Console.WriteLine( a.ToUpper() ); // 'System.Array' に 'ToUpper' の定義がありません。
+
       Console.WriteLine(s.GetType().FullName);
       Console.WriteLine(a.GetType().FullName);
+      // 出力結果
+      // 5
+      // HELLO!
+      // System.String
+      // System.Char[]
     }
   }
 }
---
+++ Script/12th/IndexerAndProperty2.cs
@@ -0,0 +1,59 @@
+// https://atmarkit.itmedia.co.jp/fdotnet/csharp_abc/csharp_abc_012/csharp_abc02.html
+
+namespace IndexerAndProperty1
+{
+    using System;
+
+    class Class2
+    {
+        private char[] a = { 'A', 'B', 'C' };
+
+        // インデクサの宣言
+        // メソッドと異なり、名前をthisにし、引数を各括弧でくくる
+        public char this[int index]
+        {
+            //インデクサにはプロパティが必要
+            get
+            {
+                return a[index];
+            }
+            set
+            {
+                a[index] = value;
+            }
+        }
+    }
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            // 【インデクサを自作する】
+
+            // 
+            Class2 t = new Class2();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(t[i]);
+            }
+            // 出力結果
+            // A
+            // B
+            // C
+
+            // データの上書き
+            t[0] = 'X';
+            t[1] = 'Y';
+            t[2] = 'Z';
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(t[i]);
+            }
+            // 出力結果
+            // X
+            // Y
+            // Z
+        }
+    }
+}
