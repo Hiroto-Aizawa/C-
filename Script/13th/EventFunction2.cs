c0b150afdd0e2e6b47cdd68d5c0b73f47ba2cdb4
Hiroto-Aizawa <r_20love@icloud.com>
2024-08-26 21:02:28 +0900

    13th 追加

--- Script/13th/EventFunction2.cs
+++ Script/13th/EventFunction2.cs
@@ -18,11 +18,34 @@ namespace EventFunction1
         }
         static void Main(string[] args)
         {
+            // 【イベントに引数を渡す】
+
             Class1 target = new Class1();
             target.sampleEvent += new SampleEventHandler(target.handler);
             SampleEventArgs sampleEventArgs = new SampleEventArgs();
             sampleEventArgs.message = "in main method";
             target.sampleEvent(target, sampleEventArgs);
+        }
+    }
+}
+
+namespace EventFunction2
+{
+    using System;
+
+    public delegate void SampleEventHandler(object sender, EventArgs e);
+    class Class1
+    {
+        public static event SampleEventHandler sampleEvent;
+        public static void handler(object o, EventArgs e)
+        {
+            Console.WriteLine("handler called");
+        }
+        static void Main(string[] args)
+        {
+            // 【staticなイベント】
+            sampleEvent += new SampleEventHandler(handler);
+            sampleEvent(null, EventArgs.Empty);
         }
     }
 }
