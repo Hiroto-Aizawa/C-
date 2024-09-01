ee990d8e50ba296d862a744efd001ee5d1684613
Hiroto-Aizawa <r_20love@icloud.com>
2024-09-01 22:23:48 +0900

    13th 追加

--- Script/13th/EventFunction3.cs
+++ Script/13th/EventFunction3.cs
@@ -2,42 +2,43 @@
 
 namespace EventFunction1
 {
-    using System;
+  using System;
 
-    public class SampleEventArgs : EventArgs
+  public class SampleEventArgs : EventArgs
+  {
+    public string message;
+  }
+  public delegate void SampleEventHandler(object sender, SampleEventArgs e);
+  class Class1
+  {
+    public event SampleEventHandler sampleEvent;
+    public void handler(object o, SampleEventArgs e)
     {
-        public string message;
+      Console.WriteLine(e.message);
     }
-    public delegate void SampleEventHandler(object sender, SampleEventArgs e);
-    class Class1
+    static void Main(string[] args)
     {
-        public event SampleEventHandler sampleEvent;
-        public void handler(object o, SampleEventArgs e)
-        {
-            Console.WriteLine(e.message);
-        }
-        static void Main(string[] args)
-        {
-            Class1 target = new Class1();
-            target.sampleEvent += new SampleEventHandler(target.handler);
-            SampleEventArgs sampleEventArgs = new SampleEventArgs();
-            sampleEventArgs.message = "in main method";
-            target.sampleEvent(target, sampleEventArgs);
-        }
+      Class1 target = new Class1();
+      target.sampleEvent += new SampleEventHandler(target.handler);
+      SampleEventArgs sampleEventArgs = new SampleEventArgs();
+      sampleEventArgs.message = "in main method";
+      target.sampleEvent(target, sampleEventArgs);
     }
+  }
 }
+
+
+using System;
 
 namespace systemLibrary
 {
-    using System;
-    
   abstract class ClassBase
   {
     public abstract void onEvenet();
   }
   class ClassLibrary
   {
-    public static void fireEvent( ClassBase instance )
+    public static void fireEvent(ClassBase instance)
     {
       instance.onEvenet();
     }
@@ -58,7 +59,46 @@ namespace userProgram
     static void Main(string[] args)
     {
       ClassDelived instance = new ClassDelived();
-      systemLibrary.ClassLibrary.fireEvent( instance );
+      systemLibrary.ClassLibrary.fireEvent(instance);
+    }
+  }
+}
+
+/// <summary>
+/// ////////////////////////////////////////////////////////////////////////////////
+/// </summary>
+using System;
+
+
+namespace systemLibrary
+{
+  public delegate void SampleEventHandler(object sender, EventArgs e);
+  class ClassLibrary
+  {
+    public static event SampleEventHandler sampleEvent;
+    public static void fireEvent()
+    {
+      sampleEvent(null, EventArgs.Empty);
+    }
+  }
+}
+
+namespace userProgram
+{
+  using systemLibrary;
+  class ClassDelived
+  {
+    public static void onEvent(object o, EventArgs e)
+    {
+      Console.WriteLine("onEvent called");
+    }
+  }
+  class Class1
+  {
+    static void Main(string[] args)
+    {
+      systemLibrary.ClassLibrary.sampleEvent += new SampleEventHandler(ClassDelived.onEvent);
+      systemLibrary.ClassLibrary.fireEvent();
     }
   }
 }
