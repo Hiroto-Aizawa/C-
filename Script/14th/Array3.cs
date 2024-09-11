e35ac565532f0ae76344309b005d7c8b69be2423
Hiroto-Aizawa <r_20love@icloud.com>
2024-09-11 15:00:42 +0900

    14th 追加

--- Script/14th/Array3.cs
+++ Script/14th/Array3.cs
@@ -1,9 +1,9 @@
 // https://atmarkit.itmedia.co.jp/fdotnet/csharp_abc/csharp_abc_014/csharp_abc03.html
 
-using System;
-
 namespace ConsoleApplication75
 {
+    using System;
+
     class Class1
     {
         private int n;
@@ -17,6 +17,23 @@ namespace ConsoleApplication75
             for (int i = 0; i < 3; i++)
             {
                 Console.WriteLine(ar[i].n);
+            }
+        }
+    }
+}
+
+namespace ConsoleApplication76
+{
+    using System;
+
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = { 0, 1, 2, 3, 4 };
+            for (int i = ar.GetLowerBound(0); i <= ar.GetUpperBound(0); i++)
+            {
+                Console.WriteLine(ar[i]);
             }
         }
     }
