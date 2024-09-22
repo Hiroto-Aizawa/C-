77576b6ff5cbc637b50641edb9e98d9e985e0bdf
Hiroto-Aizawa <r_20love@icloud.com>
2024-09-22 23:34:13 +0900

    15th 更新

--- Script/15th/Interface2.cs
+++ Script/15th/Interface2.cs
@@ -30,3 +30,54 @@ namespace Interface1
         }
     }
 }
+
+using System;
+
+namespace ConsoleApplication97
+{
+    public delegate void SampleEvent(object sender, EventArgs e);
+    public interface ISample
+    {
+        int SampleProperty { get; set; }
+        string this[int index] { get; set; }
+        event SampleEvent sampleEvent;
+    }
+    class Class1 : ISample
+    {
+        private int sample = 0;
+        public int SampleProperty
+        {
+            get { return sample; }
+            set { sample = value; }
+        }
+        private string[] ar = new string[3];
+        public string this[int index]
+        {
+            get { return ar[index]; }
+            set { ar[index] = value; }
+        }
+        public event SampleEvent sampleEvent;
+        public void handler(object sender, EventArgs e)
+        {
+            ISample isample = (ISample)sender;
+            Console.WriteLine(isample.SampleProperty);
+            Console.WriteLine(isample[0]);
+            Console.WriteLine(isample[1]);
+            Console.WriteLine(isample[2]);
+        }
+        public void invokeEvent()
+        {
+            sampleEvent(this, EventArgs.Empty);
+        }
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+            c1.SampleProperty = 123;
+            c1[0] = "ABC";
+            c1[1] = "DEF";
+            c1[2] = "GHI";
+            c1.sampleEvent += new SampleEvent(c1.handler);
+            c1.invokeEvent();
+        }
+    }
+}
