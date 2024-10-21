188ed73418e95afc6672032a7664c10d7051542e
Hiroto-Aizawa <r_20love@icloud.com>
2024-10-21 10:58:41 +0900

    15th 更新

--- Script/15th/Interface4.cs
+++ Script/15th/Interface4.cs
@@ -75,3 +75,44 @@ namespace Interfaec2
         }
     }
 }
+
+
+namespace Interface3
+{
+    using System;
+    using System.Globalization;
+
+    class ClassSample : IFormattable
+    {
+        public int x;
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (formatProvider is CultureInfo)
+            {
+                CultureInfo ci = (CultureInfo)formatProvider;
+                if (ci.Name == "ja-JP")
+                {
+                    switch (x)
+                    {
+                        case 1:
+                            return "一";
+                        case 2:
+                            return "二";
+                        case 3:
+                            return "三";
+                    }
+                }
+            }
+            return x.ToString();
+        }
+    }
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            ClassSample cs = new ClassSample();
+            cs.x = 3;
+            Console.WriteLine(cs);
+        }
+    }
+}
