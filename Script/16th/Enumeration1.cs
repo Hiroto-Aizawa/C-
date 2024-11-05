86e9bb024b8ce5f284e8b7dc508ea2565477a3ec
Hiroto-Aizawa <r_20love@icloud.com>
2024-11-05 15:30:19 +0900

    16th 追加

---
+++ Script/16th/Enumeration1.cs
@@ -0,0 +1,41 @@
+// https://atmarkit.itmedia.co.jp/fdotnet/csharp_abc/csharp_abc_016/csharp_abc01.html
+
+
+namespace Enumeration1
+{
+    using System;
+
+    enum Era
+    {
+        Meiji,
+        Taisho,
+        Showa,
+        Heisei
+    }
+    class Class1
+    {
+        static void WriteEra(Era t)
+        {
+            switch (t)
+            {
+                case Era.Meiji:
+                    Console.WriteLine("明治");
+                    break;
+                case Era.Taisho:
+                    Console.WriteLine("大正");
+                    break;
+                case Era.Showa:
+                    Console.WriteLine("昭和");
+                    break;
+                case Era.Heisei:
+                    Console.WriteLine("平成");
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Era t = Era.Taisho;
+            WriteEra(t);
+        }
+    }
+}
