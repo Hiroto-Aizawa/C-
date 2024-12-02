e3180ab24e227df0cad88d76938e23376a91c02c
Hiroto-Aizawa <r_20love@icloud.com>
2024-12-02 20:53:23 +0900

    16th 更新

--- Script/16th/Enumeration2.cs
+++ Script/16th/Enumeration2.cs
@@ -1,6 +1,6 @@
 //https://atmarkit.itmedia.co.jp/fdotnet/csharp_abc/csharp_abc_016/csharp_abc02.html
 
-namespace ConsoleApplication5
+namespace Enumeration1
 {
     using System;
 
@@ -19,6 +19,29 @@ namespace ConsoleApplication5
             Console.WriteLine((int)Era.Taisho);
             Console.WriteLine((int)Era.Showa);
             Console.WriteLine((int)Era.Heisei);
+        }
+    }
+}
+
+namespace Enumeration2
+{
+    using System;
+
+    enum Era : byte
+    {
+        Meiji,
+        Taisho,
+        Showa,
+        Heisei
+    }
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((byte)Era.Meiji);
+            Console.WriteLine((byte)Era.Taisho);
+            Console.WriteLine((byte)Era.Showa);
+            Console.WriteLine((byte)Era.Heisei);
         }
     }
 }
